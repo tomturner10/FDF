008937619fdda1e0fa9fea84a2cad4bf27121f39
Tomas Turner <tturner@F3C014.42adel.org.au>
2021-10-27 16:19:42 +1030

    done

---
+++ Makefile
@@ -0,0 +1,21 @@
+
+NAME = fdf
+
+SRCS = test.c
+
+FLAGS = -Werror -Wextra -Wall
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) -Lmlx -lmlx -framework OpenGL -framework AppKit $(SRCS) -o $(NAME)
+	
+clean:
+	rm -f $(NAME)
+
+fclean:
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ test.c
@@ -0,0 +1,35 @@
+#include "mlx/mlx.h"
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void ft_putpixel(t_data *data, int x, int y, int colour)
+{
+	char *dst;
+
+	dst = data->addr + (y + data->line_length + x * (data->bits_per_pixel / 8 ));
+	*(unsigned int*)dst = colour;
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "test");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	for (int i  = 10; i < 100; i++)
+	{
+		ft_putpixel(&img, 5, 5, 0x00FF0000);
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+}
