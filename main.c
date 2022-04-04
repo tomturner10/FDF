252a5426d4866afd65d632d0933c38bd0283c5d0
Tomas Turner <tom@ozrunways.com>
2022-04-04 15:32:47 +0930

    better

--- main.c
+++ main.c
@@ -94,14 +94,14 @@ int ft_getx(t_list *list)
 	return (count);
 }
 
-void ft_getz(t_vec3d *vec,char *z)
+void ft_getz(t_vec3d *vec, char *z)
 {
 	char **split;
 
 	split = ft_split(z, ',');
 	vec->z = ft_atoi(split[0]);
 	if (split[1])
-		vec->colour = atoi(split[1]);
+		vec->colour = ft_atoi(split[1]);
 	else
 		vec->colour = 0;
 }
@@ -119,7 +119,7 @@ t_vec3d **ft_map(t_list *list)
 		j = 0;
 		while (j < x)
 		{
-			map[(i * y) + j] = (t_vec3d *)malloc(sizeof(t_vec3d *));
+			map[(i * y) + j] = (t_vec3d *)malloc(sizeof(t_vec3d));
 			map[(i * y) + j]->y = i + 1;
 			map[(i * y) + j]->x = j + 1;
 			ft_getz(map[(i * y) + j], list->line[j]);
@@ -131,34 +131,49 @@ t_vec3d **ft_map(t_list *list)
 	return map;
 }
 
-void ft_drawline(t_img data, int x0, int y0, int x1, int y1)
-{
-	int dx, dy, p, x, y;
-	dx=x1-x0;
-	dy=y1-y0;
-	x=x0;
-	y=y0;
-	p=2*dy-dx;
-	while(x<x1)
+void ft_drawline(t_img data, int x0, int y0, int x1, int y1) {
+
+	int dx = abs(x1-x0);
+	int sx = x0<x1 ? 1 : -1;
+	int dy = abs(y1-y0);
+	int sy = y0<y1 ? 1 : -1;
+	int err = (dx>dy ? dx : -dy)/2, e2;
+
+	while(1)
 	{
-		if(p>=0)
+		ft_putpixel(&data, x0, y0, 0x00FF0000);
+		if (x0==x1 && y0==y1)
+			break;
+		e2 = err;
+		if (e2 >-dx)
 		{
-			ft_putpixel(&data,x,y,0x00FF0000);
-			y=y+1;
-			p=p+2*dy-2*dx;
+			err -= dy;
+			x0 += sx;
 		}
-		else
+		if (e2 < dy)
 		{
-			ft_putpixel(&data,x,y,0x00FF0000);
-			p=p+2*dy;}
-		x=x+1;
+			err += dx;
+			y0 += sy;
+		}
 	}
 }
 
-int toIsometric2D(int x, int y, int z, int *u, int *v){
-	*u = (x-z)/sqrt(2);
-	*v = (x+2*y+z)/sqrt(6);
-	return 0;
+static void offsetscale(t_vec3d *v)
+{
+	v->x = (win_width / 2) + (v->x * 40);
+	v->y = (win_height / 2) + (v->y * 40);
+}
+
+t_vec3d *toIsometric2D(t_vec3d *v)
+{
+	t_vec3d	*iso = (t_vec3d *)malloc(sizeof(t_vec3d));
+	iso->x = (v->x - v->y) * cos(0.523599);
+	iso->y = -v->z + (v->x + v->y) * sin(0.523599);
+	iso->z = 0.0;
+	iso->colour = v->colour;
+	offsetscale(iso);
+	printf("new: x: %f, y: %f\n", iso->x, iso->y);
+	return (iso);
 }
 
 int	main(void)
@@ -172,30 +187,22 @@ int	main(void)
 	mlx_win = mlx_new_window(mlx, win_width, win_height, "test");
 	img.img = mlx_new_image(mlx, win_width, win_height);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	list = ft_parsefdf("test_maps/20-60.fdf");
+	list = ft_parsefdf("test_maps/10-2.fdf");
 	t_vec3d **map = ft_map(list);
-	for (int i; i < 400; i++)
+	for (int i = 0; i < 99; i++)
 	{
-		printf("x : %f y: %f z: %f\n", map[i]->x, map[i]->y, map[i]->z);
-		int u = 0;
-		int v = 0;
-		toIsometric2D(map[i]->x, map[i]->y, map[i]->z, &u, &v);
-		if (i < 399) {
-			int u2 = 0;
-			int v2 = 0;
-			toIsometric2D(map[i + 1]->x, map[i + 1]->y, map[i + 1]->z, &u2, &v2);
-			ft_drawline(img, 342, 678, 789, 123);
-			//ft_drawline(img, (u * 10) + win_width/2, (v * 10) + win_height/2, (u2 * 10) + win_width/2, (v2 * 10) + win_height/2);
-			printf("u : %i u2: %i v: %i v2: %i\n", (u * 10) + win_width/2, (v * 10) + win_height/2, (u2 * 10) + win_width/2, (v2 * 10) + win_height/2);
+		if (i < 99 && (i+1)%10 != 0) {
+			t_vec3d *one= toIsometric2D(map[i]);
+			t_vec3d *two= toIsometric2D(map[i+1]);
+			ft_drawline(img, one->x, one->y, two->x, two->y);
 		}
-		if (i < 380) {
-			int u3 = 0;
-			int v3 = 0;
-			toIsometric2D(map[i + 20]->x, map[i + 20]->y, map[i + 20]->z, &u3, &v3);
-			ft_drawline(img, (u * 5) + win_width/2, (v * 5) + win_height/2, (u3 * 5) + win_width/2, (v3 * 5) + win_height/2);
+		if (i < 90) {
+			t_vec3d *one= toIsometric2D(map[i]);
+			t_vec3d *two= toIsometric2D(map[i+10]);
+			ft_drawline(img, one->x, one->y, two->x, two->y);
 		}
-		ft_putpixel(&img, (u * 5) + win_width/2, (v * 10) + win_height/2, 0x00FF0000);
-		printf("u : %i v: %i\n", u, v);
+		printf("x: %f, y: %f, z: %f\n",map[0]->x, map[0]->y, map[0]->z);
+		toIsometric2D(map[i]);
 	}
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
